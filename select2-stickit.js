0bf5054e033a0e8e49ece8d67ac1b9919bac3f31
Kevin Mudrick <kmudrick@netflix.com>
2015-03-11 10:53:37 -0700

    Support input

--- select2-stickit.js
+++ select2-stickit.js
@@ -12,7 +12,7 @@
     // Based on http://jsfiddle.net/px6UP/36/
 
     Backbone.Stickit.addHandler({
-        selector: 'select.select2',
+        selector: 'select.select2,input.select2',
         initialize: function($el, model, opt) {
             var e = 'change:' + opt.observe,
                 allowClear = $el.data("allow-clear") || false,
